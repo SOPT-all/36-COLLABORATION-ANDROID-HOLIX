7eeb92f3539c49309f97e25335e85174e4f81eb1
kangyein9892 <inwon9892@naver.com>
2025-05-13 14:37:58 +0900

    setting/#3: color 세팅

--- app/src/main/java/org/sopt/holix/core/designsystem/theme/Color.kt
+++ app/src/main/java/org/sopt/holix/core/designsystem/theme/Color.kt
@@ -1,11 +1,197 @@
 package org.sopt.holix.core.designsystem.theme
 
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary
+val MainBlue = Color(0xFF3269FF)
+val LightBlue = Color(0xFFF3F4FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Point
+val Blue = Color(0xFF004BFB)
+val SkyBlue = Color(0xFF1ABFFB)
+val Lime = Color(0xFFDEFF4E)
+val Green = Color(0xFF34C759)
+
+// Gray Scale
+val White = Color(0xFFFFFFFF)
+val Gray01 = Color(0xFFF5F5FB)
+val Gray02 = Color(0xFFEEEEF4)
+val Gray03 = Color(0xFFE0E0E6)
+val Gray04 = Color(0xFFBDBDC3)
+val Gray05 = Color(0xFF9E9EA4)
+val Gray06 = Color(0xFF616167)
+val Gray07 = Color(0xFF424248)
+val Black = Color(0xFF000000)
+
+// Background
+val Background = Color(0x4D434343)
+
+// Alert
+val AlertRed = Color(0xFFFF4040)
+val LightRed = Color(0xFFFFEDED)
+
+@Stable
+class HolixColors(
+    mainBlue: Color,
+    lightBlue: Color,
+    blue: Color,
+    skyBlue: Color,
+    lime: Color,
+    green: Color,
+    white: Color,
+    gray01: Color,
+    gray02: Color,
+    gray03: Color,
+    gray04: Color,
+    gray05: Color,
+    gray06: Color,
+    gray07: Color,
+    black: Color,
+    background: Color,
+    alertRed: Color,
+    lightRed: Color
+) {
+
+    var mainBlue: Color by mutableStateOf(mainBlue)
+        private set
+    var lightBlue: Color by mutableStateOf(lightBlue)
+        private set
+    var blue: Color by mutableStateOf(blue)
+        private set
+    var skyBlue: Color by mutableStateOf(skyBlue)
+        private set
+    var lime: Color by mutableStateOf(lime)
+        private set
+    var green: Color by mutableStateOf(green)
+        private set
+    var white: Color by mutableStateOf(white)
+        private set
+    var gray01: Color by mutableStateOf(gray01)
+        private set
+    var gray02: Color by mutableStateOf(gray02)
+        private set
+    var gray03: Color by mutableStateOf(gray03)
+        private set
+    var gray04: Color by mutableStateOf(gray04)
+        private set
+    var gray05: Color by mutableStateOf(gray05)
+        private set
+    var gray06: Color by mutableStateOf(gray06)
+        private set
+    var gray07: Color by mutableStateOf(gray07)
+        private set
+    var black: Color by mutableStateOf(black)
+        private set
+    var background: Color by mutableStateOf(background)
+        private set
+    var alertRed: Color by mutableStateOf(alertRed)
+        private set
+    var lightRed: Color by mutableStateOf(lightRed)
+        private set
+
+    fun copy(
+        mainBlue: Color = this.mainBlue,
+        lightBlue: Color = this.lightBlue,
+        blue: Color = this.blue,
+        skyBlue: Color = this.skyBlue,
+        lime: Color = this.lime,
+        green: Color = this.green,
+        white: Color = this.white,
+        gray01: Color = this.gray01,
+        gray02: Color = this.gray02,
+        gray03: Color = this.gray03,
+        gray04: Color = this.gray04,
+        gray05: Color = this.gray05,
+        gray06: Color = this.gray06,
+        gray07: Color = this.gray07,
+        black: Color = this.black,
+        background: Color = this.background,
+        alertRed: Color = this.alertRed,
+        lightRed: Color = this.lightRed
+    ): HolixColors = HolixColors(
+        mainBlue = mainBlue,
+        lightBlue = lightBlue,
+        blue = blue,
+        skyBlue = skyBlue,
+        lime = lime,
+        green = green,
+        white = white,
+        gray01 = gray01,
+        gray02 = gray02,
+        gray03 = gray03,
+        gray04 = gray04,
+        gray05 = gray05,
+        gray06 = gray06,
+        gray07 = gray07,
+        black = black,
+        background = background,
+        alertRed = alertRed,
+        lightRed = lightRed
+    )
+
+    fun update(other: HolixColors) {
+        mainBlue = other.mainBlue
+        lightBlue = other.lightBlue
+        blue = other.blue
+        skyBlue = other.skyBlue
+        lime = other.lime
+        green = other.green
+        white = other.white
+        gray01 = other.gray01
+        gray02 = other.gray02
+        gray03 = other.gray03
+        gray04 = other.gray04
+        gray05 = other.gray05
+        gray06 = other.gray06
+        gray07 = other.gray07
+        black = other.black
+        background = other.background
+        alertRed = other.alertRed
+        lightRed = other.lightRed
+    }
+}
+
+fun holixColors(
+    mainBlue: Color = MainBlue,
+    lightBlue: Color = LightBlue,
+    blue: Color = Blue,
+    skyBlue: Color = SkyBlue,
+    lime: Color = Lime,
+    green: Color = Green,
+    white: Color = White,
+    gray01: Color = Gray01,
+    gray02: Color = Gray02,
+    gray03: Color = Gray03,
+    gray04: Color = Gray04,
+    gray05: Color = Gray05,
+    gray06: Color = Gray06,
+    gray07: Color = Gray07,
+    black: Color = Black,
+    background: Color = Background,
+    alertRed: Color = AlertRed,
+    lightRed: Color = LightRed
+) = HolixColors(
+    mainBlue,
+    lightBlue,
+    blue,
+    skyBlue,
+    lime,
+    green,
+    white,
+    gray01,
+    gray02,
+    gray03,
+    gray04,
+    gray05,
+    gray06,
+    gray07,
+    black,
+    background,
+    alertRed,
+    lightRed
+)
+
