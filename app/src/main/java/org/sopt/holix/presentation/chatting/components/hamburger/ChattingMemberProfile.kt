e35dd4771dc684035aad6259989a1dd3010f55cc
sonms <smsms5676@naver.com>
2025-05-15 02:26:25 +0900

    chore/#9:채팅 햄버거 멤버 프로필 프리뷰 추가

--- app/src/main/java/org/sopt/holix/presentation/chatting/components/hamburger/ChattingMemberProfile.kt
+++ app/src/main/java/org/sopt/holix/presentation/chatting/components/hamburger/ChattingMemberProfile.kt
@@ -13,9 +13,11 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import org.sopt.holix.R
+import org.sopt.holix.core.designsystem.theme.HolixAndroidTheme
 
 @Composable
 fun ChattingMemberProfile(
@@ -51,5 +53,17 @@ fun ChattingMemberProfile(
                 modifier = modifier.align(Alignment.BottomEnd)
             )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChattingMemberProfilePreview() {
+    HolixAndroidTheme {
+        ChattingMemberProfile(
+            imageUrl = "",
+            isOwner = true,
+            isActive = true
+        )
     }
 }
