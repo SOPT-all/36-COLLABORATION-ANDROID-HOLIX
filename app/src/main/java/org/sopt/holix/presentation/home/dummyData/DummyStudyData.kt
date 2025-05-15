3d4d4f25b6ed77ec9d3c483448b47eb4d280fa8d
Madcamp2 <navygrace8389@gmail.com>
2025-05-16 04:48:49 +0900

    더미 데이터

--- app/src/main/java/org/sopt/holix/presentation/home/dummyData/DummyStudyData.kt
+++ app/src/main/java/org/sopt/holix/presentation/home/dummyData/DummyStudyData.kt
@@ -9,7 +9,7 @@ val dummyStudyList1 = listOf(
         studyTitle = "위클리 UX / UI 분석 챌린지",
         studyLeader = "레오",
         price = "매월 14,900원",
-        imageUrl = "https://example.com/img1.jpg",
+        imageUrl = "",
         tags = listOf(
             TagUiModel("챌린지", "blue"),
             TagUiModel("커뮤니티", "black")
@@ -21,7 +21,7 @@ val dummyStudyList1 = listOf(
         studyTitle = "매주 재무제표 50가지 공부하기",
         studyLeader = "재무제표 읽는 남자",
         price = "매월 23,900원",
-        imageUrl = "https://example.com/img2.jpg",
+        imageUrl = "",
         tags = listOf(
             TagUiModel("온라인", "blue")
         ),
@@ -35,7 +35,7 @@ val dummyStudyList2 = listOf(
         studyTitle = "최정문 북클럽 - 과학으로 가는 문(과학, 수학, 공학, SF 독서클럽)",
         studyLeader = "최정문",
         price = "매월 14,900원",
-        imageUrl = "https://example.com/img3.jpg",
+        imageUrl = "",
         tags = listOf(
             TagUiModel(name = "온라인", color = "blue"),
             TagUiModel(name = "커뮤니티", color = "gray")
@@ -47,7 +47,7 @@ val dummyStudyList2 = listOf(
         studyTitle = "🚀 스타트업 로켓 성장",
         studyLeader = "최인철 / Jordan",
         price = "매월 100,000원",
-        imageUrl = "https://example.com/img4.jpg",
+        imageUrl = "",
         tags = listOf(
             TagUiModel(name = "커뮤니티", color = "gray")
         ),
