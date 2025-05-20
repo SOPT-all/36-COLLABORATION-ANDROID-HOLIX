b0e3cfe8bfce215e8fec9767d6a16ce97c931b88
mjth1s1s <navygrace8389@gmail.com>
2025-05-20 14:52:59 +0900

    add: 도메인 모델 구조 수정

--- app/src/main/java/org/sopt/holix/data/dto/response/StudyReponseDto.kt
+++ app/src/main/java/org/sopt/holix/data/dto/response/StudyReponseDto.kt
@@ -1,11 +1,55 @@
 package org.sopt.holix.data.dto.response
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
+@Serializable
 data class StudyResponseDto(
+    @SerialName("code")
+    val code: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: StudyDataDto
+)
+
+@Serializable
+data class StudyDataDto(
+    @SerialName("passionateStudies")
+    val passionateStudies: List<StudyDto>,
+    @SerialName("insightStudies")
+    val insightStudies: List<StudyDto>,
+    @SerialName("newStudies")
+    val newStudies: List<StudyDto>,
+    @SerialName("recommendStudies")
+    val recommendStudies: List<StudyDto>,
+    @SerialName("freeStudies")
+    val freeStudies: List<StudyDto>
+)
+
+@Serializable
+data class StudyDto(
+    @SerialName("studyId")
     val studyId: Int,
+    @SerialName("studyTitle")
     val studyTitle: String,
+    @SerialName("studyLeader")
     val studyLeader: String,
+    @SerialName("price")
     val price: String,
+    @SerialName("url")
     val url: String,
-    val tags: List<TagResponseDto>,
+    @SerialName("tags")
+    val tags: List<TagDto>,
+    @SerialName("category")
     val category: String
+)
+@Serializable
+data class TagDto(
+    @SerialName("tagId")
+    val tagId: Int,
+    @SerialName("tagName")
+    val tagName: String,
+    @SerialName("tagColor")
+    val tagColor: String
 )
--- app/src/main/java/org/sopt/holix/data/dto/response/TagResponseDto.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.holix.data.dto.response
-
-data class TagResponseDto(
-    val tagId: Int,
-    val tagName: String,
-    val tagColor: String
-)
