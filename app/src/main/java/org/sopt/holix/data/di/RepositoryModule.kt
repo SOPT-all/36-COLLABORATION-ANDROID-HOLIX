1cec926c913d6f5b257a9cd8552bd7c87769c326
kangyein9892 <inwon9892@naver.com>
2025-05-21 12:28:50 +0900

    setting#20: repository 세팅

--- app/src/main/java/org/sopt/holix/data/di/RepositoryModule.kt
+++ app/src/main/java/org/sopt/holix/data/di/RepositoryModule.kt
@@ -4,12 +4,22 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.sopt.holix.data.repository.ClubRepositoryImpl
 import org.sopt.holix.data.repository.DummyRepositoryImpl
+import org.sopt.holix.data.repository.MainRepositoryImpl
+import org.sopt.holix.domain.repository.ClubRepository
 import org.sopt.holix.domain.repository.DummyRepository
+import org.sopt.holix.domain.repository.MainRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface RepositoryModule {
     @Binds
     fun bindDummyRepository(dummyRepositoryImpl: DummyRepositoryImpl): DummyRepository
+
+    @Binds
+    fun bindMainRepository(mainRepositoryImpl: MainRepositoryImpl): MainRepository
+
+    @Binds
+    fun bindClubRepository(clubRepositoryImpl: ClubRepositoryImpl): ClubRepository
 }
