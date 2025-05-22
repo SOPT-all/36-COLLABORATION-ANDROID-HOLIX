16f4a0842763dfde4ecf3122d5141e047afe52e8
sonms <smsms5676@naver.com>
2025-05-22 19:41:17 +0900

    feat/#24: 채팅 DataSource 구현

--- app/src/main/java/org/sopt/holix/data/datasource/ChattingDataSource.kt
+++ app/src/main/java/org/sopt/holix/data/datasource/ChattingDataSource.kt
@@ -1,10 +1,13 @@
 package org.sopt.holix.data.datasource
 
+import org.sopt.holix.data.dto.request.ChattingRequestDto
 import org.sopt.holix.data.service.ChattingService
 import javax.inject.Inject
 
 class ChattingDataSource @Inject constructor(
     private val chattingService: ChattingService
 ) {
-    suspend fun getChattingList(clubId : Long) = chattingService.getChattingList(clubId)
+    suspend fun getChattingList(clubId: Long) = chattingService.getChattingList(clubId)
+
+    suspend fun postChatting(clubId: Long, chattingRequestDto: ChattingRequestDto) = chattingService.postChatting(clubId, chattingRequestDto)
 }
