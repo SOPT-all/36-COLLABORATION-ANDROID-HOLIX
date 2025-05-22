fd38c40ae3113c479f294a3bcf4a57340f91606e
sonms <smsms5676@naver.com>
2025-05-22 21:35:56 +0900

    chore/#24: 채팅 state 관리 수정, viewmodel - post 메세지 기능, get wrapper로 변경하면서 수정

--- app/src/main/java/org/sopt/holix/presentation/chatting/ChattingViewModel.kt
+++ app/src/main/java/org/sopt/holix/presentation/chatting/ChattingViewModel.kt
@@ -1,5 +1,6 @@
 package org.sopt.holix.presentation.chatting
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,6 +12,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.sopt.holix.core.util.UiState
 import org.sopt.holix.core.util.handleError
+import org.sopt.holix.data.dto.request.ChattingRequestDto
 import org.sopt.holix.domain.repository.ChattingRepository
 import javax.inject.Inject
 
@@ -29,16 +31,50 @@ class ChattingViewModel @Inject constructor(
     fun getChattingList(clubId: Long) = viewModelScope.launch {
         chattingRepository.getChattingList(clubId = clubId)
             .onSuccess {
+                val chattingList = it.chattingList.map { chatting ->
+                    chatting.toDomainModel()
+                }
+                Log.d("chattingList", chattingList.toString())
                 _state.value = _state.value.copy(
-                    uiState = UiState.Success(it.toPersistentList())
+                    uiState = UiState.Success(chattingList.toPersistentList())
                 )
             }.onFailure { throwable ->
                 val errorMessage = handleError(throwable)
                 _state.value = _state.value.copy(
                     uiState = UiState.Failure(errorMessage)
                 )
+                Log.e("getChattingList", "getChattingList: $errorMessage")
                 showSnackBar(errorMessage)
             }
+    }
+
+    fun postChatting(clubId: Long, chattingRequestDto: ChattingRequestDto) = viewModelScope.launch {
+        chattingRepository.postChatting(clubId, chattingRequestDto)
+            .onSuccess {
+                _state.value = _state.value.copy(
+                    chattingState = UiState.Success(it.message)
+                )
+            }
+            .onFailure { throwable ->
+                val errorMessage = handleError(throwable)
+                _state.value = _state.value.copy(
+                    chattingState = UiState.Failure(errorMessage)
+                )
+                Log.e("postChatting", "postChatting: $errorMessage")
+                showSnackBar(errorMessage)
+            }
+    }
+
+    fun fetchText(chat: String) = viewModelScope.launch {
+        _state.value = _state.value.copy(
+            chattingText = chat
+        )
+    }
+
+    fun sendChatting() = viewModelScope.launch {
+        _state.value = _state.value.copy(
+            isSendChatting = !state.value.isSendChatting
+        )
     }
 
     fun showSnackBar(message: String) = viewModelScope.launch {
