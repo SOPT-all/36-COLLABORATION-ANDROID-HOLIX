965cd35e07d8b9a29a6e38ad94d312450284f38d
sonms <smsms5676@naver.com>
2025-05-22 21:27:57 +0900

    feat/#24: 채팅, 채팅햄버거 네비게이션 구현

---
+++ app/src/main/java/org/sopt/holix/presentation/chatting/navigation/ChattingDetailHamburgerNavigation.kt
@@ -0,0 +1,28 @@
+package org.sopt.holix.presentation.chatting.navigation
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import org.sopt.holix.core.navigation.Route
+import org.sopt.holix.presentation.chatting.ChattingHamburgerRoute
+
+fun NavController.navigateToChattingHamburger(navOptions: NavOptions? = null) = navigate(Route.ChattingDetailHamburger, navOptions)
+
+fun NavGraphBuilder.chattingHamburgerNavGraph(
+    navigateUp: () -> Unit,
+    navigateNext: () -> Unit,
+    snackBarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+) {
+    composable<Route.ChattingDetailHamburger> {
+        ChattingHamburgerRoute(
+            navigateUp = navigateUp,
+            navigateNext = navigateNext,
+            snackBarHostState = snackBarHostState,
+            modifier = modifier
+        )
+    }
+}
