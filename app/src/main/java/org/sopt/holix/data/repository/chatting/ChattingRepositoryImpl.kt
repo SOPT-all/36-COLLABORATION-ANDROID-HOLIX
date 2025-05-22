0a10d9fe5dacea833b7d5bd5c951eb8ee8e9ee63
sonms <smsms5676@naver.com>
2025-05-22 19:41:54 +0900

    feat/#24: 채팅 repository 구현체 구현

--- app/src/main/java/org/sopt/holix/data/repository/chatting/ChattingRepositoryImpl.kt
+++ app/src/main/java/org/sopt/holix/data/repository/chatting/ChattingRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package org.sopt.holix.data.repository.chatting
 
 import org.sopt.holix.data.datasource.ChattingDataSource
+import org.sopt.holix.data.dto.request.ChattingRequestDto
+import org.sopt.holix.data.dto.response.chatting.ChattingListWrapperDto
+import org.sopt.holix.data.dto.response.chatting.ChattingPostResponseDto
 import org.sopt.holix.domain.model.chatting.ChattingListDataEntity
 import org.sopt.holix.domain.repository.ChattingRepository
 import javax.inject.Inject
@@ -8,7 +11,11 @@ import javax.inject.Inject
 class ChattingRepositoryImpl @Inject constructor(
     private val chattingDataSource: ChattingDataSource
 ) : ChattingRepository {
-    override suspend fun getChattingList(clubId: Long): Result<List<ChattingListDataEntity>> = runCatching {
-        chattingDataSource.getChattingList(clubId).data.map { it.toDomainModel() }
+    override suspend fun getChattingList(clubId: Long): Result<ChattingListWrapperDto> = runCatching {
+        chattingDataSource.getChattingList(clubId).data
+    }
+
+    override suspend fun postChatting(clubId: Long, chattingRequestDto: ChattingRequestDto): Result<ChattingPostResponseDto> = runCatching {
+        chattingDataSource.postChatting(clubId, chattingRequestDto)
     }
 }
