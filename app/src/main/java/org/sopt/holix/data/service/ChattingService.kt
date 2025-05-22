10743193ed91e44b26694687eb95208c2bd15035
sonms <smsms5676@naver.com>
2025-05-22 19:42:23 +0900

    feat/#24: 채팅 서비스 - 채팅리스트, 채팅전송 구현

--- app/src/main/java/org/sopt/holix/data/service/ChattingService.kt
+++ app/src/main/java/org/sopt/holix/data/service/ChattingService.kt
@@ -1,13 +1,24 @@
 package org.sopt.holix.data.service
 
-import org.sopt.holix.data.dto.response.chatting.ChattingBaseResponse
-import org.sopt.holix.data.dto.response.chatting.ChattingResponseDto
+import org.sopt.holix.data.dto.request.ChattingRequestDto
+import org.sopt.holix.data.dto.response.BaseResponse
+import org.sopt.holix.data.dto.response.chatting.ChattingListWrapperDto
+import org.sopt.holix.data.dto.response.chatting.ChattingPostResponseDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ChattingService {
-    @GET("/club/{clubId}/chat")
+    @GET("api/v1/club/{clubId}/chat")
     suspend fun getChattingList(
         @Path(value = "clubId") clubId : Long
-    ) : ChattingBaseResponse<ChattingResponseDto>
+    ) : BaseResponse<ChattingListWrapperDto>
+
+
+    @POST("api/v1/club/{clubId}/chat")
+    suspend fun postChatting(
+        @Path(value = "clubId") clubId : Long,
+        @Body chattingRequest: ChattingRequestDto
+    ) : ChattingPostResponseDto
 }
